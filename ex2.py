5b5214514ff8b28a88b55d8facb95f5a125499b7
Alon Bronshtein <allen.bronshtein@gmail.com>
2021-11-17 22:48:39 +0200

    loaded trainset

--- ex2.py
+++ ex2.py
@@ -1,2 +1,18 @@
 import numpy as np
-print("hello world!")
+
+X = np.genfromtxt('PA-SVM-Perceptron-KNN/train_x.txt', delimiter=',')
+Y = np.genfromtxt('PA-SVM-Perceptron-KNN/train_y.txt')
+test_X = np.genfromtxt('PA-SVM-Perceptron-KNN/test_x.txt', delimiter=',')
+
+
+def knn(X,Y):
+    print("unimplemented knn")
+
+def passive_agressive(X,Y):
+    print("unimplemented pa")
+
+def svm(X,Y):
+    print("unimplemented svm")
+
+def perceptron(X,Y):
+    print("unimplemented perceptron")
