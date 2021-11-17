d113090efe9fac62bf321bf935627a7998e4b3c2
Alon Bronshtein <allen.bronshtein@gmail.com>
2021-11-17 23:35:06 +0200

    normalization added

--- ex2.py
+++ ex2.py
@@ -1,18 +1,33 @@
 import numpy as np
+TEST_SIZE = 0
 
-X = np.genfromtxt('PA-SVM-Perceptron-KNN/train_x.txt', delimiter=',')
-Y = np.genfromtxt('PA-SVM-Perceptron-KNN/train_y.txt')
-test_X = np.genfromtxt('PA-SVM-Perceptron-KNN/test_x.txt', delimiter=',')
+def normalize(x,test_x):
+    # min-max normalization
+    max_v = np.amax(x, axis=0)
+    min_v = np.amin(x, axis=0)
+    for i in range(240):
+        for j in range(5):
+            x[i][j] = (x[i][j] - min_v[j]) / (max_v[j] - min_v[j])
+            if i < TEST_SIZE:
+                test_x[i][j] = (test_x[i][j] - min_v[j]) / (max_v[j] - min_v[j])
 
-
-def knn(X,Y):
+def knn(x,y):
     print("unimplemented knn")
 
-def passive_agressive(X,Y):
+def passive_agressive(x,y):
     print("unimplemented pa")
 
-def svm(X,Y):
+def svm(x,y):
     print("unimplemented svm")
 
-def perceptron(X,Y):
+def perceptron(x,y):
     print("unimplemented perceptron")
+
+def loss(x,y,w):
+    print("unimplemeted loss")
+
+x = np.genfromtxt('PA-SVM-Perceptron-KNN/train_x.txt', delimiter=',')
+y = np.genfromtxt('PA-SVM-Perceptron-KNN/train_y.txt')
+test_x = np.genfromtxt('PA-SVM-Perceptron-KNN/test_x.txt', delimiter=',')
+TEST_SIZE = test_x.size/5
+normalize(x,test_x)
