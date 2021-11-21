f453a10deed5eb0d287e4dbbceff0516e4e70794
Alon Bronshtein <allen.bronshtein@gmail.com>
2021-11-21 20:46:08 +0200

    knn change

--- ex2.py
+++ ex2.py
@@ -1,13 +1,15 @@
 import numpy as np
+from operator import itemgetter
 
+# Globals
 TRAIN_X_DIR = 'PA-SVM-Perceptron-KNN/train_x.txt'
 TRAIN_Y_DIR = 'PA-SVM-Perceptron-KNN/train_y.txt'
 TEST_X_DIR = 'PA-SVM-Perceptron-KNN/test_x.txt'
 OUTPUT_DIR = 'PA-SVM-Perceptron-KNN/output.txt'
+
+
 # min-max normalization
-
-
-def normalize(train_x, test_x):
+def normalize():
     max_v, min_v = np.amax(train_x, axis=0), np.amin(train_x, axis=0)
     for i in range(train_size):
         for j in range(num_att):
@@ -17,68 +19,63 @@ def normalize(train_x, test_x):
                     (max_v[j] - min_v[j])
 
 
+# Shuffle training set
+def shuffle():
+    np.random.shuffle(s)
+    train_x_shuffled = train_x[s]
+    train_y_shuffled = train_y[s]
+    return zip(train_x_shuffled, train_y_shuffled)
+
+
+def log():
+    msg = ''
+    for i in range(test_size):
+        msg += f"knn: {knn_test_y[i]}, perceptron: {perceptron_test_y[i]}, svm: 0, pa: {pa_test_y[i]}\n"
+    _out = open(OUTPUT_DIR, "w")
+    _out.write(msg)
+    _out.close()
+
+
 # KNN learning algorithm
-def knn(train_x, train_y, test_x):
+def knn():
     KN = 7
-    test_y = []
-    # find the distance for each test x from every train x
-    for x in test_x:
-        dist = []
-        classes = {'0': 0, '1': 0, '2': 0}
-        # save distance and matching label in a pair
-        for i in range(test_size):
-            dist.append([np.linalg.norm(x - train_x[i]), train_y[i]])
-        dist_np = np.array(dist)
-        # find k nearest neighbors
-        sorted_array = dist_np[np.argsort(dist_np[:, 0])]
-        nearest_neighbors = sorted_array[:KN]
-        # find the number of occurrences for each class
-        for line in range(KN):
-            classes[str(int(nearest_neighbors[line][1]))] += 1
-        # predict the majority class and assign to current x example
-        max_class = max(classes, key=classes.get)
-        test_y.append(int(max_class))
-    return test_y
+    for x_test in test_x:
+        dist, classes = [], {0: 0, 1: 0, 2: 0}
+        for i in range(train_size):
+            dist.append([np.linalg.norm(x_test - train_x[i]), train_y[i]])
+        dist.sort(key=itemgetter(0))
+        nearest_neighbors = dist[:KN]
+        for i in range(KN):
+            classes[nearest_neighbors[i][1]] += 1
+        knn_test_y.append(max(classes, key=classes.get))
 
 
 # Perceptron learning algorithm
-def perceptron(train_x, train_y, test_x):
-    LEARNING_RATE = 0.2
-    EPOCHS = 20
+def perceptron():
+    LEARNING_RATE, EPOCHS = 0.2, 20
     w = np.array([np.zeros(num_att), np.zeros(num_att), np.zeros(num_att)])
-    test_y = []
     for _ in range(EPOCHS):
-        # shuffle train data every epoch
-        s = np.arange(train_x.shape[0])
-        np.random.shuffle(s)
-        train_x_shuffled = train_x[s]
-        train_y_shuffled = train_y[s]
-        # predict and update
-        for x_i, y_i in zip(train_x_shuffled, train_y_shuffled):
-            y_hat = np.argmax(np.dot(w, x_i))
-            if y_hat != int(y_i):
-                w[int(y_i), :] = w[int(y_i), :] + LEARNING_RATE * x_i
-                w[y_hat, :] = w[y_hat, :] - LEARNING_RATE * x_i
+        train_set = shuffle()
+        for x, y in train_set:
+            y_hat = np.argmax(np.dot(w, x))
+            if y_hat != y:
+                w[y, :] = w[y, :] + LEARNING_RATE * x
+                w[y_hat, :] = w[y_hat, :] - LEARNING_RATE * x
     # use the trained weight vectors to assign best label prediction to current x example
-    for x in range(test_size):
-        test_y.append(np.argmax(np.dot(w, test_x[x])))
-    return test_y
+    for i in range(test_size):
+        perceptron_test_y.append(np.argmax(np.dot(w, test_x[i])))
 
 
 # Passive agressive learning algorithm
-def passive_agressive(train_x, train_y, test_x):
+def passive_agressive():
     EPOCHS = 1
     w = np.array([np.random.rand(num_att), np.random.rand(
         num_att), np.random.rand(num_att)])
     test_y = []
     for _ in range(EPOCHS):
-        # shuffle train data every epoch
-        s = np.arange(train_x.shape[0])
-        np.random.shuffle(s)
-        train_x_shuffled = train_x[s]
-        train_y_shuffled = train_y[s]
+        train_set = shuffle()
         # predict and update
-        for x_i, y_i in zip(train_x_shuffled, train_y_shuffled):
+        for x_i, y_i in train_set:
             w_out = np.delete(w, int(y_i), 0)
             y_hat = np.argmax(np.dot(w_out, x_i))
             # get the correct label by it's place in original w
@@ -101,7 +98,7 @@ def passive_agressive(train_x, train_y, test_x):
 
 
 # SVM learning algorithm
-def svm(train_x, train_y, test_x):
+def svm():
     test_y = []
     return test_y
 
@@ -111,20 +108,17 @@ train_x = np.genfromtxt(TRAIN_X_DIR, delimiter=',')
 train_y = np.genfromtxt(TRAIN_Y_DIR)
 test_x = np.genfromtxt(TEST_X_DIR, delimiter=',')
 num_att = train_x[0].size
-train_size = int(train_x.size/num_att)
-test_size = int(test_x.size/num_att)
+train_size = train_x.shape[0]
+test_size = test_x.shape[0]
+s = np.arange(train_x.shape[0])
 knn_test_y, perceptron_test_y, svm_test_y, pa_test_y = [], [], [], []
 
 
-normalize(train_x, test_x)
-knn_test_y = knn(train_x, train_y, test_x)
-perceptron_test_y = perceptron(train_x, train_y, test_x)
-pa_test_y = passive_agressive(train_x, train_y, test_x)
+normalize()
+knn()
+perceptron()
+passive_agressive()
+svm()
 
-msg = ''
-for i in range(test_size):
-    msg += f"knn: {knn_test_y[i]}, perceptron: {perceptron_test_y[i]}, svm: 0, pa: {pa_test_y[i]}\n"
-_out = open(OUTPUT_DIR, "w")
-_out.write(msg)
-_out.close()
+log()
 # End
